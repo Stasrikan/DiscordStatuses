ae9b467fade0c18015aa68eb60287f058ec2f057
Stasrikan <44445171+Stasrikan@users.noreply.github.com>
2020-06-14 12:11:37 +0300

    01-login
    Added the ability to log into your account

---
+++ build/precache-manifest.09747aff87aa489be95ffeca3546435c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acbcf37ad8e28fa72bd73d2f49698131",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b1dccf0611f4295dc6ce",
+    "url": "./static/css/main.ccf578ac.chunk.css"
+  },
+  {
+    "revision": "457a993d4d76c01fad6f",
+    "url": "./static/js/2.b58fcbdb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.b58fcbdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1dccf0611f4295dc6ce",
+    "url": "./static/js/main.02e664b6.chunk.js"
+  },
+  {
+    "revision": "59b77ab7a6bf0fe26a8e",
+    "url": "./static/js/runtime-main.c2b37d53.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
